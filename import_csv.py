e1adc3301ab20cf40e177d49cb0e990680896557
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-02-16 18:06:14 +0100

    import csv script
    small script to import csv files
    
    in this first step the columns need
    to be article and software
    
    in the future this can be extended to the same
    format as quick statements uses

---
+++ import_csv.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+import requests
+import csv
+
+from mardiclient import MardiClient
+
+mc = MardiClient(user=os.environ.get('MARDI_USER'), password=os.environ.get('MARDI_PASSWORD'))
+
+
+def import_csv(url):
+    with requests.get(url, stream=True) as r:
+        lines = (line.decode('utf-8') for line in r.iter_lines())
+        for row in csv.DictReader(lines):
+            process_row(row)
+
+
+def process_row(row):
+    try:
+        article = mc.item.get(entity_id=row['article'])
+        article.add_claim('wdt:P223', 'wd:' + row['software'])
+        article.write()
+    except Exception as error:
+        print(row, error, file=sys.stderr)
+
+
+if __name__ == '__main__':
+    import_csv(sys.argv[1])
